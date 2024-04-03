24c550d5f58fd75a39dcc89b8a2fe4c2ec998988
Guguitino <adamguglielmino@gmail.com>
2024-04-03 15:11:27 +0200

    maj Environnement.py

--- Environnement.py
+++ Environnement.py
@@ -7,13 +7,19 @@ class Environment():
         self.width = width
         self.height = height
         self.grid = np.zeros((height, width))
-        self.fourmis = np.array([])
+        self.fourmis = []
 
     
     def addFourmi(self, fourmi):
         self.fourmis.append(fourmi)
         self.grid[fourmi.x][fourmi.y] = fourmi
 
+    def step(self):           
+        print("step")
+        self.step()
+        
+
 
 env = Environment(4, 2)
+
 print(env.grid)
