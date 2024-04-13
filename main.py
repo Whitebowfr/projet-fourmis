34bdbee02309166e149ef60169f776b830fb6639
Whitebow <whitebowfr@gmail.com>
2024-04-13 20:02:41 +0200

    Fixed startup lag

--- main.py
+++ main.py
@@ -3,35 +3,38 @@ from display import Display
 from ants import Ants
 import taichi as ti
 import time
+import constants
 
 ti.init()
 
 class FourmiSim :
     def __init__(self, width, height, N) :
-        self.env = Environment(width, height, 2)
+        self.env = Environment(width, height)
         self.ants = Ants(N, self.env.grid, self.env.home, self.env.food)
-        ti.sync()
-        self.display = Display(width, height, 2, self.env.home)
-        self.previous_update = time.time()
+        #ti.sync()
+        self.display = Display(width, height, 1, self.env.home)
+        self.display.hide_markers = True
         self.i = 0
+        self.updateFourmis(1e-5)
         ti.sync()
-        self.display.update_grid(self.env.grid, self.ants, self.env.food)
+        self.display.update_grid(self.env.grid, self.ants.positions, self.env.food)
+        ti.sync()
 
     def update_game(self) :
         deltaT = time.time() - self.previous_update
         self.previous_update = time.time()
         self.env.decay(deltaT)
-        if self.i % 10 == 0 :
+        if self.i % ti.static(constants.SPREAD_RATE) == 0 :
             self.env.box_blur()
         self.updateFourmis(deltaT)
-        ti.sync()
-        self.display.update_grid(self.env.grid, self.ants, self.env.food)
+        self.display.update_grid(self.env.grid, self.ants.positions, self.env.food)
         self.i += 1
     
     def updateFourmis(self, deltaT:float) :
         self.ants.update(deltaT)
 
     def run(self) :
+        self.previous_update = time.time()
         while True :
             self.update_game()
 
@@ -40,6 +43,6 @@ class FourmiSim :
     
 
 if __name__ == "__main__" :
-    sim = FourmiSim(700, 700, 500)
+    sim = FourmiSim(700, 700, 1000)
     sim.run()
             
