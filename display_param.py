a82b9ff5218792e0e5fd3dfbe252649f33b03e85
lchardin <louis.chardin71@gmail.com>
2024-04-23 11:28:13 +0200

    affichage des zones de capteurs

--- display_param.py
+++ display_param.py
@@ -48,7 +48,7 @@ class Display_param(tk.Tk):
 
         self.val_sens_angle = tk.DoubleVar()
         self.val_sens_angle.set(45)
-        self.sens_angle = tk.Scale(self.left_zone, orient = 'horizontal', from_= 0, to = 100, resolution = 1, tickinterval=20, label='Valeur SENSOR ANGLE', variable= self.val_sens_angle)
+        self.sens_angle = tk.Scale(self.left_zone, orient = 'horizontal', from_= 5, to = 45, resolution = 1, tickinterval=5, label='Valeur SENSOR ANGLE', variable= self.val_sens_angle)
         self.sens_angle.pack(fill = 'x')
 
         self.val_turn_spd = tk.DoubleVar()
@@ -65,22 +65,50 @@ class Display_param(tk.Tk):
         self.canevas.pack(anchor="se")
 
         self.display_canvas()
+        self.refresh()
 
     def recover(self, event):
-        self.SENSOR_OFFSET_DISTANCE = self.val_sens_offset_dist.get()
-        self.SENSOR_SIZE = self.val_sens_size.get()
-        self.SENSOR_ANGLE_DEGREES = self.val_sens_angle.get()
-        self.TURN_SPEED = self.val_turn_spd.get()
-        self.MOVE_SPEED = self.val_mv_spd.get()
+        list_chgt = []
+        self.SENSOR_OFFSET_DISTANCE = int(self.val_sens_offset_dist.get())
+        self.SENSOR_SIZE = int(self.val_sens_size.get())
+        self.SENSOR_ANGLE_DEGREES = int(self.val_sens_angle.get())
+        self.TURN_SPEED = int(self.val_turn_spd.get())
+        self.MOVE_SPEED = int(self.val_mv_spd.get())
+        new_list = [self.SENSOR_OFFSET_DISTANCE, self.SENSOR_SIZE, self.SENSOR_ANGLE_DEGREES, self.TURN_SPEED, self.MOVE_SPEED]
+        if self.list_param:
+            for i in range(len(self.list_param)):
+                if new_list[i] != self.list_param[i]:
+                    list_chgt.append(i)
+        # print(f'offset : {self.SENSOR_OFFSET_DISTANCE}, size : {self.SENSOR_SIZE}, angle : {self.SENSOR_ANGLE_DEGREES}, turn : {self.TURN_SPEED}, move : {self.MOVE_SPEED}')
+        self.list_param = new_list
 
-        print(f'offset : {self.SENSOR_OFFSET_DISTANCE}, size : {self.SENSOR_SIZE}, angle : {self.SENSOR_ANGLE_DEGREES}, turn : {self.TURN_SPEED}, move : {self.MOVE_SPEED}')
+        return list_chgt
 
     def display_canvas(self):
-    #     self.canevas.create_oval([10, 10], [210, 110], fill='black')
-    #     self.canevas.create_oval([200, 40], [250, 80], fill='black')
-    #     self.canevas.create_oval([250, 20], [350, 100], fill='black')
+        list_chgt = self.recover('<Button-1>')
         self.canevas.create_image([400, 250], image=self.img_fourmi)
-        self.canevas.create_arc([500, 150], [700, 350], extent=2*int(self.val_sens_angle.get()), start = 90-int(self.val_sens_angle.get()), fill='red')
+        
+        self.id_arc_av = self.canevas.create_arc([500, 157], [700, 357], extent=2*self.SENSOR_ANGLE_DEGREES, start= -self.SENSOR_ANGLE_DEGREES, fill='red')
+        self.id_arc_g = self.canevas.create_arc([500, 157], [700, 357], extent=2*self.SENSOR_ANGLE_DEGREES, start= 90-self.SENSOR_ANGLE_DEGREES, fill='red')
+        self.id_arc_d = self.canevas.create_arc([500, 157], [700, 357], extent=2*self.SENSOR_ANGLE_DEGREES, start = 270-self.SENSOR_ANGLE_DEGREES, fill='red')
+
+    def refresh(self):
+        list_chgt = self.recover('<Button-1>')
+
+        if 0 in list_chgt:
+            self.canevas.itemconfigure(self.id_arc_av)
+            self.canevas.itemconfigure(self.id_arc_g)
+            self.canevas.itemconfigure(self.id_arc_d)
+        if 1 in list_chgt:
+            self.canevas.itemconfigure(self.id_arc_av)
+            self.canevas.itemconfigure(self.id_arc_g)
+            self.canevas.itemconfigure(self.id_arc_d)
+        if 2 in list_chgt:
+            self.canevas.itemconfigure(self.id_arc_av, extent=2*self.SENSOR_ANGLE_DEGREES, start=-self.SENSOR_ANGLE_DEGREES)
+            self.canevas.itemconfigure(self.id_arc_g, extent=2*self.SENSOR_ANGLE_DEGREES, start=90-self.SENSOR_ANGLE_DEGREES)
+            self.canevas.itemconfigure(self.id_arc_d, extent=2*self.SENSOR_ANGLE_DEGREES, start=270-self.SENSOR_ANGLE_DEGREES)
+
+        self.after(100, self.refresh)
 
 if __name__ == "__main__":
     app = Display_param()
