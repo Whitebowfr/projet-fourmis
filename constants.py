b639d04b1dd889af147cd094418e25e93cd815e7
Whitebow <whitebowfr@gmail.com>
2024-06-08 15:11:46 +0200

    Added backtrack

--- constants.py
+++ constants.py
@@ -1,35 +1,35 @@
 DECAY_RATE = 0.5
-SPREAD_RATE: int = 1
+SPREAD_RATE: int = 10
 
 NUMBER_OF_PHEROMONES: int = 2
 
-SENSOR_OFFSET_DISTANCE: int = 2
-SENSOR_SIZE: int = 45
+SENSOR_OFFSET_DISTANCE: int = 15
+SENSOR_SIZE: int = 5
 SENSOR_ANGLE_RAD: float = 45 * 3.14 / 180
 LOST_SPEED: float = 0.3
-DETECTION_RANGE: int = 7
+DETECTION_RANGE: int = 5
 
-TURN_SPEED: float = 150  * 3.14 / 180
+TURN_SPEED: float = 150 * 3.14/180
 MOVE_SPEED: int = 150
 RANDOM_FACT: int = 5
 
-HOME_SIZE: int = 50
+HOME_SIZE: int = 20
 FOOD_SIZE: int = 20
 FOOD_COUNT: int = 20
 
+BG_BRIGHTNESS = 0.5
 
 HIDE_MARKERS: bool = False
 
 TRESHOLD: float = 0.1
+BACK_TURN_FORCE: float = 0.3
 
 colors = {"fourmis": "#FF0000",
           "food": "#00FF00",
           "maison": "#FFA500",
           "obstacles": "#646464",
-          "pheromones": ["#0000FF", "#FFFF00"]}
+          "pheromones": ["#FF0000", "#0000FF"]}
 
 DEFAULT_PAINT_SIZE: int = 20
 
 HOME_ZOOM_FACTOR: int = 1
-
-BG_BRIGHTNESS: float = 0.4
