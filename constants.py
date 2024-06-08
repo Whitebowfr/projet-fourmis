ed0a94395dc87ed1f45f7a4e5a62f04713e2b09c
lchardin <louis.chardin71@gmail.com>
2024-06-08 16:49:51 +0200

    test de main menu

--- constants.py
+++ constants.py
@@ -11,11 +11,11 @@ DETECTION_RANGE: int = 5
 
 TURN_SPEED: float = 150  * 3.14 / 180
 MOVE_SPEED: int = 150
-RANDOM_FACT: int = 15
+RANDOM_FACT: int = 5
 
-HOME_SIZE: int = 20
-FOOD_SIZE: int = 20
-FOOD_COUNT: int = 20
+HOME_SIZE: int = 10
+FOOD_SIZE: int = 10
+FOOD_COUNT: int = 10
 
 BG_BRIGHTNESS:float = 0.5
 
@@ -24,7 +24,7 @@ HIDE_MARKERS: bool = False
 TRESHOLD: float = 0.1
 BACK_TURN_FORCE: float = 0.3
 
-colors = {"fourmis": "#FF0000","food": "#00FF00","maison": "#FFA500","obstacles": "#646464","pheromones": ["#FF0000", "#00FFFF"]}
+colors = {'fourmis': '#00ffff', 'food': '#00FF00', 'maison': '#FFA500', 'obstacles': '#646464', 'pheromones': ['#FF0000', '#00FFFF']}
 
 DEFAULT_PAINT_SIZE: int = 20
 
